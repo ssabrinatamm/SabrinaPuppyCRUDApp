b2106e2580ae2016a2897952d73eff8af68488ba
Sabrina Tam <sabrinatam03@gmail.com>
2026-03-31 00:19:47 -0500

    debugging

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -10,12 +10,12 @@
 // )
 
 
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { AsgardeoProvider } from '@asgardeo/react'
-// import { AuthProvider } from '@asgardeo/react'
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.jsx'
+// import { AsgardeoProvider } from '@asgardeo/react'
+// // import { AuthProvider } from '@asgardeo/react'
 
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
