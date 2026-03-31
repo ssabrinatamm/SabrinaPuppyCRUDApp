cca68234a42dc8458baed40e7ad306c69e71a2b6
Sabrina Tam <sabrinatam03@gmail.com>
2026-03-30 23:25:58 -0500

    debugging frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
-import Header from "./components/header";
-import Footer from "./components/footer";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Page2 from "./pages/Page2";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
